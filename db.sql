5e63e955cb8cf5faa35237674e86c030afd1deb9
Serg Serov <owchin.s@yandex.ru>
2022-12-14 11:50:28 +0300

    add user table

---
+++ db.sql
@@ -0,0 +1,29 @@
+create schema if not exists habr_app;
+
+create table habr_app."user"
+(
+    id                       bigserial
+        constraint user_pk
+            primary key,
+    name                     varchar(60)               not null,
+    nickname                 varchar(60)               not null
+        constraint user_nickname_u
+            unique,
+    hashed_password          varchar(60)               not null,
+    about_info               text,
+    datetime_of_registration timestamptz default now() not null,
+    img_src                  varchar(60)
+);
+
+comment on column habr_app."user".name is 'name of user';
+
+comment on column habr_app."user".nickname is 'unique @nickname of user';
+
+comment on column habr_app."user".hashed_password is 'bcrypt password hash';
+
+comment on column habr_app."user".about_info is 'some info about user';
+
+comment on column habr_app."user".datetime_of_registration is 'date and time of user registration';
+
+comment on column habr_app."user".img_src is 'uri of user img src';
+
---
+++ init_data.sql
@@ -0,0 +1,4 @@
+insert into habr_app."user"("name", nickname, hashed_password, about_info)
+values ('Овчинников Сергей', 'serov', '$2a$12$noxi.d0SiUz5CRmrDXY2MeHqtDxBUkLBLZ/p8UIMg2lbHrw0Yep4C', 'разработчик и студент'),
+       ('Кутузов Андрей', 'dev_podcast', '$2a$12$Zrnw5tNnrjSgpvV.xNkt1erQ5tdFvATbtO4hJjl6Ap1u0LPKxlR6.', 'ведущий канала на ютубе'),
+       ('Никита Бя', 'hike_man', '$2a$12$BAZlbb8n2HIDGRwjZ1JDN.9GqByvB5vHN8lwsaIFFOQjnaN2HPUF6', 'IT рекрутер');

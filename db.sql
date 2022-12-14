3e6c2a4fb4f62e38375f93a1496b6c8a08976930
Serg Serov <owchin.s@yandex.ru>
2022-12-14 12:01:32 +0300

    add carma and rating

--- db.sql
+++ db.sql
@@ -12,7 +12,9 @@ create table habr_app."user"
     hashed_password          varchar(60)               not null,
     about_info               text,
     datetime_of_registration timestamptz default now() not null,
-    img_src                  varchar(60)
+    img_src                  varchar(60),
+    carma                    int4 default 0 check ( carma >= 0 ),
+    rating                   float4 default 0.0 check ( rating >= 0.0 )
 );
 
 comment on column habr_app."user".name is 'name of user';
@@ -26,4 +28,8 @@ comment on column habr_app."user".about_info is 'some info about user';
 comment on column habr_app."user".datetime_of_registration is 'date and time of user registration';
 
 comment on column habr_app."user".img_src is 'uri of user img src';
+
+comment on column habr_app."user".carma is 'user carma';
+
+comment on column habr_app."user".rating is 'user rating';
 
